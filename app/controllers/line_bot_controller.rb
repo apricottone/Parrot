50a2e441cacc50bbeebf8be61686d901fb29f9c1
apricottone <pencil3274@gmail.com>
2022-01-14 06:42:12 +0900

    implemented reply function

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,6 +1,32 @@
 class LineBotController < ApplicationController
+    protect_from_forgery except: [:callback]
+    
     def callback
+        # 署名の検証用
+        body = request.body.read
+        # ヘッダー内の署名
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        # 署名の検証
+        unless client.validate_signature(body, signature)
+            return head :bad_request # false => ERROR:400
+        end
 
+        events = client.parse_events_from(body) # 配列化
+        events.each do |event|
+            case event
+            when Line::Bot::Event::Message
+                case event.type
+                when Line::Bot::Event::MessageType::Text
+                    # テキストメッセージの抽出
+                    message = {
+                        type: 'text',
+                        text: event.message['text']
+                    }
+                    client.reply_message(event['replyToken'], message)
+                end
+            end
+        end
+        head :ok # status_code: 200
     end
 
     private
